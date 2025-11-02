a85d492b1f66ee0c8de8bd787a2e382ffc41cc07
Marcin Sikorski <marcin.sikorski.dev@gmail.com>
2025-11-02 23:51:26 +0100

    Update api main.go file with new Resolver
    Relates to: #5

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -7,33 +7,54 @@ import (
 
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/handler/extension"
-	"github.com/99designs/gqlgen/graphql/handler/lru"
 	"github.com/99designs/gqlgen/graphql/handler/transport"
 	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/SkMarcin/Token-Transfer-API/graph"
-	"github.com/vektah/gqlparser/v2/ast"
+	"github.com/SkMarcin/Token-Transfer-API/internal/core"
+	"github.com/SkMarcin/Token-Transfer-API/internal/database"
+	"github.com/joho/godotenv"
 )
 
 const defaultPort = "8080"
 
 func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = defaultPort
 	}
 
-	srv := handler.New(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
+	db, err := database.Connect()
+	if err != nil {
+		log.Fatalf("ERROR: Could not connect to DB: %v", err)
+	}
+
+	if err := database.RunMigrations(db); err != nil {
+		log.Fatalf("ERROR: Migration failed: %v", err)
+	}
+
+	if err := database.SeedInitialBalance(db); err != nil {
+		log.Fatalf("ERROR: Seeding failed: %v", err)
+	}
+
+	walletService := core.NewWalletService(db)
+
+	srv := handler.NewDefaultServer(graph.NewExecutableSchema(
+		graph.Config{
+			Resolvers: &graph.Resolver{
+				WalletService: walletService,
+			},
+		},
+	))
 
 	srv.AddTransport(transport.Options{})
-	srv.AddTransport(transport.GET{})
 	srv.AddTransport(transport.POST{})
 
-	srv.SetQueryCache(lru.New[*ast.QueryDocument](1000))
-
 	srv.Use(extension.Introspection{})
-	srv.Use(extension.AutomaticPersistedQuery{
-		Cache: lru.New[string](100),
-	})
 
 	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
 	http.Handle("/query", srv)
